0749e3149b945e517af5ecfd0b6ce0743cd7c70e
Sterling <sterling1@hotmail.ca>
2023-11-08 23:44:43 -0500

    Added comments and extracted string resources

--- app/src/main/java/com/example/assignment2/LocationDao.kt
+++ app/src/main/java/com/example/assignment2/LocationDao.kt
@@ -10,22 +10,13 @@ interface LocationDao {
     @Query("SELECT * FROM location")
     fun getAll(): List<Location>
 
-/*    @Query("SELECT * FROM location WHERE uid IN (:userIds)")
-    fun loadAllByIds(userIds: IntArray): List<Location>*/
-
-/*    @Query("SELECT * FROM location WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    fun findByName(first: String, last: String): Location*/
-
     @Query("SELECT * FROM location WHERE address LIKE :address LIMIT 1")
     fun findByAddress(address: String): Location
 
     @Insert
     fun insert(location: Location)
 
-    @Insert
-    fun insertAll(vararg locations: Location)
 
-/*    @Delete
-    fun delete(user: Location)*/
+    @Delete
+    fun delete(location: Location)
 }
--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -14,6 +14,7 @@ import java.io.InputStreamReader
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    //Using room for database as recommended by Android Developers official website
     private lateinit var db: LocationRoomDatabase
     private lateinit var locationDao: LocationDao
 
@@ -29,6 +30,10 @@ class MainActivity : AppCompatActivity() {
         populateDatabase()
     }
 
+    /*
+    A function for populating the database with addresses that are found from
+    looking at the coordinates in the Locations.txt file
+     */
     private fun populateDatabase(){
         val reader = BufferedReader(
             InputStreamReader(assets.open("Locations.txt"))
@@ -36,39 +41,56 @@ class MainActivity : AppCompatActivity() {
 
         var line: String? = reader.readLine();
         var id = 1
+
+        //Goes over every line in Locations.txt
         while(line != null){
+            //Split each line into latitude and longitude
             val arr = line.split(", ")
             val lat = arr[0].toDouble()
             val long = arr[1].toDouble()
+
+            //Get the address from the coordinates to address function
             val addressObj = coordinatesToAddress(lat, long)
+            println(addressObj)
+
+            //Format address to be saved in the database
             var address: String
             if(addressObj != null){
                 address = "${addressObj.countryName}, ${addressObj.adminArea}, ${addressObj.locality}, ${addressObj.getAddressLine(0)}"
             }else{
                 continue
             }
+
             val location = Location(id = id++, address = address, latitude = lat, longitude = long)
-            println("Location is: $location")
-            locationDao.insert(location)
+//            println("Location is: $location")
+//            locationDao.insert(location) //Currently causes errors
 
             line = reader.readLine()
         }
     }
 
+    /*Takes in a latitude and longitude value and returns a single Address object
+    that comes from the geoCoder list
+     */
     private fun coordinatesToAddress(lat: Double, long:Double): Address?{
         val geocoder: Geocoder = Geocoder(this, Locale.CANADA)
         val addressList = geocoder.getFromLocation(lat, long, 1)
         return addressList?.get(0) ?: null
     }
 
+    /*
+    Function for the search button
+    Searches for an address within the database
+     */
     fun searchForLocation(v: View){
         val searchBar = findViewById<EditText>(R.id.searchBar)
         val search = searchBar.text.toString()
 
+        //Shows a toast message if the user did not enter an address to search
         if(search == ""){
             Toast.makeText(
                 this,
-                "Enter something to search it",
+                getString(R.string.empty_search_error),
                 Toast.LENGTH_LONG
             ).show()
             return
@@ -78,6 +100,10 @@ class MainActivity : AppCompatActivity() {
         displayResults(location)
     }
 
+    /*
+    Displays the results of the search in the activity by
+    which includes the address, latitude, and longitude saved
+    */
     private fun displayResults(location: Location){
         val textAddress = findViewById<TextView>(R.id.textAddress)
         val textLatitude = findViewById<TextView>(R.id.textLatitude)
